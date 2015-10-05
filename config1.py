7630a46461ee6229eab1e900389825e9dd1ffd6c
i-am-Q <qbenbow@gmail.com>
2015-10-04 20:40:50 -0400

    Necesary files for ava

---
+++ config1.py
@@ -0,0 +1,19 @@
+from base64 import b64encode
+
+# name that wakes up system
+my_name = ['ava']
+
+# email username that is used to send notifications
+mail_sender = 'Send_emails_from_this_address'
+# password for sender
+mail_access = b64encode('Your_Password_Here')
+# who to send emails to
+mail_list = ['Where_you_would_Like_To_Recieve_Emails']
+
+# wolfram alpha key
+wolf_key = 'wolfram_key'
+# wunderground weather service key
+wunder_key = 'wunderground_key'
+
+# to do list location
+todo_loc = 'todolist_loation'
